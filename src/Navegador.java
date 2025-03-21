ddd5df2628709a4d19990d2075588302fe008363
Alisson <Alissonsouzac@hotmail.com>
2025-03-21 19:30:40 -0300

    Colocados atributos hitorico e paginaAtual na classe Navegador

--- src/Navegador.java
+++ src/Navegador.java
@@ -1,2 +1,9 @@
 public class Navegador {
+    private Historico historico;
+    private String paginaAtual;
+
+    public Navegador () {
+        this.historico = new Historico();
+        this.paginaAtual = null;
+    }
 }
