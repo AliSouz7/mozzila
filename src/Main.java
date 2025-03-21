0b1fc50e0d3b1e9842db1efe122d31ed0810fd57
Alisson <Alissonsouzac@hotmail.com>
2025-03-21 19:50:13 -0300

    Adicionado o menu de navegação usando a estrutura de repetição while

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,29 @@
+import java.util.Scanner;
 public class Main {
+    public static void main(String[]args) {
+        Scanner scanner = new Scanner(System.in);
+        Navegador navegador = new Navegador();
+
+    //O menu vai se repetir até que a opcão de sair seja selecionada
+
+        while (true) {
+            System.out.println("MENU - NAVEGADOR");
+            System.out.println("1 - Nova pagina");
+            System.out.println("2 - Voltar");
+            System.out.println("3 - Avncar");
+            System.out.println("4 - Sair");
+            System.out.print(" ESCOLHA UMA OPCAO: ");
+
+            int opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcao) {
+                case 1: System.out.print("Digite a URL: ");
+                String url = scanner.nextLine();
+                navegador.visitarPagina(url);
+                break;
+            }
+
+        }
+    }
 }
